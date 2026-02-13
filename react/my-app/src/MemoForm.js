a48ed8fe2814c42f174c09d077bbdfb0c01e9bcc
Shota Saito <s.shota0408@gmail.com>
2026-02-13 15:05:00 +0900

    memoapp_in_local

---
+++ react/my-app/src/MemoForm.js
@@ -0,0 +1,53 @@
+const MemoForm = ({
+  title,
+  setTitle,
+  content,
+  setContent,
+  tag,
+  setTag,
+  onSave,
+}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        padding: 20,
+        border: "1px solid #61dafb",
+        marginTop: 20,
+        width: 300,
+      }}
+    >
+      <label>
+        タイトル
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+
+      <label>
+        内容
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </label>
+
+      <label>
+        タグ
+        <input
+          type="text"
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+        />
+      </label>
+
+      <button onClick={onSave}>保存</button>
+    </div>
+  );
+};
+
+export default MemoForm;
